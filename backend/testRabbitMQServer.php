4639b159f4a280a0df4bd7908a5fd4861fc97005
Mikey <mhp9@njit.edu>
2026-02-26 16:58:17 +0000

    fixing queues

--- backend/testRabbitMQServer.php
+++ backend/testRabbitMQServer.php
@@ -69,9 +69,8 @@ echo "routing key: " . print_r($request, true) . PHP_EOL;
     return ['ok' => false, 'error' => 'Unsupported type']; 
 }
 
-$ini = isset($argv[1]) ? $argv[1] : "testRabbitMQ.ini";
-$server = new rabbitMQServer($ini, "testServer");
+
+$server = new rabbitMQServer("testRabbitMQ.ini", "testServer");
 $server->process_requests('requestProcessor');
 exit();
 ?>
-
