a2c7f83228d8d26aef3c432bc982a7848ef7ecad
Mikey <mhp9@njit.edu>
2026-02-26 17:21:01 +0000

    fixing

--- webserver/login.php
+++ webserver/login.php
@@ -18,7 +18,7 @@ if ($username === '' || $password === '') {
     exit;
 }
 
-$client = new rabbitMQClient("testRabbitMQ_login.ini", "testServer");
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
 $request = [
     'type' => 'login',
