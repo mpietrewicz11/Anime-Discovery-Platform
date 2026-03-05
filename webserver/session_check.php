827de719783f8d886e61f75ef2b329a3e431d7f4
emihidaki-12 <emihidaki1998@gmail.com>
2026-03-05 00:14:13 -0500

    Deleted test files (so we dont get confused)

--- webserver/session_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-header("Content-Type: text/plain");
-
-echo "Session ID: " . session_id() . "\n";
-echo "Username: " . ($_SESSION['username'] ?? "(not set)") . "\n";
-echo "All session data:\n";
-print_r($_SESSION);
