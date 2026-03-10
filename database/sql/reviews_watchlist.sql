0969433c86b16377d00d23ae8798ee60f78f8f0d
AJ Estrella <ae396@njit.edu>
2026-03-10 11:42:30 -0400

    Made a small change from list_id to id instead

--- database/sql/reviews_watchlist.sql
+++ database/sql/reviews_watchlist.sql
@@ -13,13 +13,13 @@ CREATE TABLE user_reviews (
 );
 --watch list table
 CREATE TABLE watch_list (
-  list_id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT,
   user_id int NOT NULL,
   anime_id int NOT NULL,
   title varchar(255) NOT NULL,
   status enum('plan_of_watch','watching','done','hold','dropped') DEFAULT 'plan_of_watch',
   added_at datetime DEFAULT CURRENT_TIMESTAMP,
   updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (list_id),
+  PRIMARY KEY (id),
   UNIQUE KEY user_id (user_id,anime_id)
 );
