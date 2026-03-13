18d09093546b776f51ef59e908c627614b39a60f
AJ Estrella <123426216+ae396@users.noreply.github.com>
2026-03-13 13:34:53 -0400

    users.sql with comments

--- database/sql/users.sql
+++ database/sql/users.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
-  username varchar(50) NOT NULL,
+  id int NOT NULL AUTO_INCREMENT, -- this is the unique user identifier
+  username varchar(50) NOT NULL, -- the ability to have a unique username for the login
   password varchar(255) DEFAULT NULL,
-  email varchar(255) NOT NULL,
-  email_notifications tinyint(1) DEFAULT '1',
+  email varchar(255) NOT NULL, -- this is the users email address
+  email_notifications tinyint(1) DEFAULT '1', -- the 1 is enabled for notifications while as 0 is disabled
   PRIMARY KEY (id),
-  UNIQUE KEY username (username)
+  UNIQUE KEY username (username) -- this actually prevents multiple or duplicate usernames
 );
