c2f3907a92cc6c169e7d462f8278d54e4e933e22
AJ Estrella <123426216+ae396@users.noreply.github.com>
2026-03-14 22:01:32 -0400

    Update anime.sql

--- database/sql/anime.sql
+++ database/sql/anime.sql
@@ -1,8 +1,8 @@
 CREATE TABLE anime (
-  anime_id int NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL,
-  score decimal(3,1) NOT NULL,
-  episodes int DEFAULT NULL,
+  anime_id int NOT NULL AUTO_INCREMENT, -- provides a unique id to each anime within the database
+  title varchar(255) NOT NULL, -- the title of the anime
+  score decimal(3,1) NOT NULL, -- the score/rating for example 6.8
+  episodes int DEFAULT NULL, -- the amount of episodes the anime has
   updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (anime_id)
 );
