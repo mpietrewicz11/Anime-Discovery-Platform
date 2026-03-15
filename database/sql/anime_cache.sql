5021bad4dc8d04265c61b4a27ad39959d242b0ea
AJ Estrella <123426216+ae396@users.noreply.github.com>
2026-03-14 21:55:53 -0400

    anime_cache.sql comments

--- database/sql/anime_cache.sql
+++ database/sql/anime_cache.sql
@@ -1,10 +1,10 @@
 CREATE TABLE anime_cache (
-  mal_id int NOT NULL,
+  mal_id int NOT NULL, -- this is the MyAnimeList id an external API 
   title varchar(255) DEFAULT NULL,
-  poster text,
-  score float DEFAULT NULL,
-  year int DEFAULT NULL,
-  genre varchar(100) DEFAULT NULL,
+  poster text, -- this is the photo/poster link to the image
+  score float DEFAULT NULL, -- this is the rating score from the API
+  year int DEFAULT NULL, -- the year the anime released
+  genre varchar(100) DEFAULT NULL, -- the genre of the anime
   updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (mal_id)
 );
