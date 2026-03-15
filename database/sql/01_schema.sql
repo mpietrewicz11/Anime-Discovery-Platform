01c875d8a5f59a0498e9887768622913b35a83dd
AJ Estrella <123426216+ae396@users.noreply.github.com>
2026-03-14 22:04:06 -0400

    Update 01_schema.sql

--- database/sql/01_schema.sql
+++ database/sql/01_schema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS it490;
 USE it490;
 CREATE TABLE IF NOT EXISTS users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	username VARCHAR(50) NOT NULL UNIQUE,
+	id INT AUTO_INCREMENT PRIMARY KEY, -- a unique identifier for each of the users
+	username VARCHAR(50) NOT NULL UNIQUE, -- username that is going to be used for the login
 	password VARCHAR(50) NOT NULL );
