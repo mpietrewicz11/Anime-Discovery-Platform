7c8a12e613fa69eee331d6cf0ecc061496ddec56
Mikey <mhp9@njit.edu>
2026-04-09 19:28:48 +0000

    Add deployClient

---
+++ deploy/deployClient.php
@@ -0,0 +1,21 @@
+<?php
+require_once('backend/rabbitMQLib.inc');
+
+$act = $argv[1] ?? ";
+$tgt = $argv [2] ?? ";
+$f = $argv[3] ?? ";
+$v = $argv[4] ?? ";
+
+if (!$act || !$tgt) die("usage: php deployClient.php <deploy|rollback|list> <qa|prod> [file] [ver]\n");
+
+$c = new rabbitMQClient("backend/deploy.ini", "deployServer");
+$req = ['type' => $act, 'target' => $tgt];
+if ($act === 'deploy') {
+	$req['bundle_name'] = "adem";
+	$req['version'] = $v;
+	$req['filename'] = $f;
+}
+
+print_r($c->send_request($req));
+?>
+	
