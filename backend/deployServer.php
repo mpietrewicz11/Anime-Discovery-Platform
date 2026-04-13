125d5b7b172261b76471184b62fc135ba4d4aa5d
Mikey <mhp9@njit.edu>
2026-04-13 01:57:15 +0000

    Changed database name to deploy

--- backend/deployServer.php
+++ backend/deployServer.php
@@ -1,7 +1,7 @@
 <?php
 require_once('rabbitMQLib.inc');
 
-$db = new mysqli("127.0.0.1", "deploy", "123", "deploy_db");
+$db = new mysqli("127.0.0.1", "deploy", "123", "deploy");
 if ($db->connect_error) die ("db connection failed\n");
 
 function updateBundle($db, $id, $status) {
