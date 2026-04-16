5f242a707a4c9a8108393685c7747f34151e3f51
AJ Estrella <ae396@njit.edu>
2026-04-16 12:48:49 -0400

    Made more changes, decentralized is getting to me

--- backend/decentralizedLogger.php
+++ backend/decentralizedLogger.php
@@ -6,26 +6,42 @@ $params = [
 	'password' => 'guest',
 	'vhost' => '/'
 ];
+try {
 $conn = new AMQPConnection($params);
 $conn->connect();
 $channel = new AMQPChannel($conn);
 $exchange = new AMQPExchange($channel);
-$exchange->setName("logs_exchange");
+$exchange->setName("logs.exchange");
 $exchange->setType("fanout");
+$exchange->declareExchange();
 
 $queue = new AMQPQueue($channel);
 $queue->setName("");
 $queue->setFlags(AMQP_EXCLUSIVE);
-$queue->declare();
-$queue->bind("logs_exchange");
+$queue->declareQueue();
+$queue->bind("logs.exchange");
 
 echo "Listening for any logs...\n";
 
 $queue->consume(function($msg) {
-	file_put_contents(
-		"logs.txt"
-		$msg->getBody() . PHP_EOL,
-		FILE_APPEND
-	);
+	$payload = [
+		"time" => date("c"),
+		"lvl" => "INFO",
+		"message" => $msg->getBody(),
+		"service" => "ServiceName",
+		"host" => gethostname(),
+		"user_id" => "user_id_any"
+	];
+	$logEntry = json_encode($payload, JSON_PRETTY_PRINT) . PHP_EOL;
+
+	try {
+		file_put_contents("loggings.txt", $logEntry, FILE_APPEND);
+	} catch (Exception $e) {
+		echo "Error of writing to a logging file: " . $e->getMessage() . "\n";
+	} 
+	
 });
+} catch (Exception $e) {
+	echo "Error message: " . $e->getMessage() . "\n";
+}
 ?>
