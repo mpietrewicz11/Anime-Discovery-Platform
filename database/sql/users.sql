a51ce0efbf732f8b5af0fa65955be5bf0b3e7e28
AJ Estrella <ae396@njit.edu>
2026-05-04 17:03:48 -0400

    Proof of bio being text in users table because I have to manually do it nano for github

--- database/sql/users.sql
+++ database/sql/users.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
   password varchar(255) DEFAULT NULL,
   email varchar(255) NOT NULL, -- this is the users email address
   email_notifications tinyint(1) DEFAULT '1', -- the 1 is enabled for notifications while as 0 is disabled
+  bio text,
   PRIMARY KEY (id),
   UNIQUE KEY username (username) -- this actually prevents multiple or duplicate usernames
 );
